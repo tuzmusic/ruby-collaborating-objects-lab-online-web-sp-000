21afede9485365e126e935c1c287d4257fa19841
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-12 19:20:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,17 +21,15 @@ class Artist
     @songs << song
   end
 
-end
+  def self.find_or_create_by_name(name)
+    # if @@all contains the artist, return it
+    # if not, create it
 
-def self.find_or_create_by_name(name)
-  # if @@all contains the artist, return it
-  # if not, create it
+    existing_artist = @@all.find { |artist|
+      artist.name == name
+    }
+    return existing_artist if existing_artist
 
-  existing_artist = @@all.find { |artist|
-    artist.name == name
-  }
-  return existing_artist if existing_artist
-
-  Artist.new(name)
+    Artist.new(name)
 
 end
