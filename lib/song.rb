70416e588296da9bfac926d07fc53aa0c7115986
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-12 19:33:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,16 @@ class Song
 
   attr_accessor :name, :artist
 
-  def initialize(name)
+  def initialize(name, artist)
     @name = name
+    @artist = artist
   end
 
   def new_by_filename(filename)
-
+    components = filename.split(" - ")
+    artist = components[0]
+    title = components[1]
+    Song.new
   end
 
 
