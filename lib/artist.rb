a9269a6cfdcb8d240e922ebd411bbd24d1b7eef8
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-12 19:09:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,16 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all
 
   def initialize(name)
     @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
   end
 
 end
